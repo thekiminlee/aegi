79ad2d7cbfc202b3aa8e737345e1d0191069ca47
thekiminlee <thekiminlee@gmail.com>
2026-05-14 10:15:34 -0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:aegi/app/app.dart';
+import 'package:aegi/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Lock orientation
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  // Firebase initialization
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: Aegi()));
 }
